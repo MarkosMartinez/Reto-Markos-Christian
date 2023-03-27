99b4c1e0697d90126a7b7941fdb5750e53eda43b
MarkosMartinez <ikbdk@plaiaundi.net>
2023-03-27 14:22:34 +0200

    Conector creado y primera clase rellenada y el SQL a medias

--- src/main/java/modelo/DAO/Conector.java
+++ src/main/java/modelo/DAO/Conector.java
@@ -1,5 +1,48 @@
 package modelo.DAO;
 
-public class Conector {
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
+public class Conector {
+	
+	private Connection conexion;
+	
+	private static final String HOST = "localhost";
+	private static final String BBDD = "reto-3eva";
+	private static final String USERNAME = "root";
+	private static final String PASSWORD = "";
+	
+	public void conectar() {
+		
+		try {
+			
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			conexion = DriverManager.getConnection("jdbc:mysql://" + HOST + "/" + BBDD, USERNAME, PASSWORD);
+
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public void cerrar() {
+		try {
+			conexion.close();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	
+	//Getters Y Setters
+	
+	public Connection getCon() {
+		return conexion;
+	}
+
+	public void setCon(Connection con) {
+		this.conexion = con;
+	}
+	//Fin de Getters y Setters
 }
--- src/main/java/modelo/DTO/Clinica.java
+++ src/main/java/modelo/DTO/Clinica.java
@@ -1,5 +1,41 @@
 package modelo.DTO;
 
 public class Clinica {
+	
+	private int id_clinica;
+	private String nombre_clinica;
+	private String direccion;
+	private int telefono;
+	
+	public Clinica() {
+		
+	}
+	
+	public int getId_clinica() {
+		return id_clinica;
+	}
+	public void setId_clinica(int id_clinica) {
+		this.id_clinica = id_clinica;
+	}
+	public String getNombre_clinica() {
+		return nombre_clinica;
+	}
+	public void setNombre_clinica(String nombre_clinica) {
+		this.nombre_clinica = nombre_clinica;
+	}
+	public String getDireccion() {
+		return direccion;
+	}
+	public void setDireccion(String direccion) {
+		this.direccion = direccion;
+	}
+	public int getTelefono() {
+		return telefono;
+	}
+	public void setTelefono(int telefono) {
+		this.telefono = telefono;
+	}
+	
+	
 
 }
