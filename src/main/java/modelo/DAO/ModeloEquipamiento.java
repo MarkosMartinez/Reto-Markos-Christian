e2b6a7d031ac961b02f320f6e70bad2476a2d976
Chris2609 <megustas260904@gmail.com>
2023-05-13 19:22:01 +0200

    documentando el codigo para el javadoc

--- src/main/java/modelo/DAO/ModeloEquipamiento.java
+++ src/main/java/modelo/DAO/ModeloEquipamiento.java
@@ -15,6 +15,12 @@ public class ModeloEquipamiento {
 		this.con = con;
 	}
 
+	/**
+	 * Obtiene toda la lista de equipamiento de la clinica especificada que hay en la tabla "equipamiento" 
+	 * 
+	 * @param id_Clinica es el ID de la clinica de la cual se quiere obtener el equipamiento
+	 * @return devuelve un ArrayList con todo el equipamiento botenido de la clinica que se ha especificado
+	 */
 	public ArrayList<Equipamiento> cargarEquipamiento(int id_Clinica) {
 		ArrayList<Equipamiento> equipamiento = new ArrayList<>();
 		try {
@@ -37,12 +43,20 @@ public class ModeloEquipamiento {
 		return equipamiento;
 	}
 
+	/**
+	 * Actualiza el stock de todo el equipamiento, tanto si se ha cambiaod el valor como si no
+	 * 
+	 * @param equipamiento es un ArrayList con los ID de cada equipamiento
+	 * @return si se actualiza correctamente el stock del equipamiento devuelve un boolean llamado
+	 * "actualizado" con el valor "true", en caso de que ocurra algun error en la actualizacion se 
+	 * devolverá con el valor "false 
+	 */
 	public boolean actualizarTodos(ArrayList<Equipamiento> equipamiento) {
 		boolean actualizado = true;
 		for (Equipamiento equip : equipamiento) {
 			PreparedStatement pstModificar;
 			try {
-	            pstModificar = this.con.getCon().prepareStatement("UPDATE equipamiento SET Stock = ? WHERE ID_Equipamiento = ?;");
+	            pstModificar = this.con.getCon().prepareStatement("UPDATE equipamiento SET Stock = ? WHERE ID_Equipamiento = ?");
 	            pstModificar.setInt(1, equip.getStock());
 	            pstModificar.setInt(2, equip.getId_Equipamiento());
 	            pstModificar.execute();
@@ -55,6 +69,15 @@ public class ModeloEquipamiento {
 		return actualizado;
 	}
 	
+	/**
+	 * Actualiza el stock de solo un equipamiento
+	 * 
+	 * @param id_Equipamiento es el ID del equipamiento del que se ha actualizado el stock
+	 * @param nuevoStock es la nueva cantidad de stock del equipamiento que se ha cambiado
+	 * @return si se actualiza correctamente el stock del equipamiento devuelve un boolean llamado
+	 * "actualizado" con el valor "true", en caso de que ocurra algun error en la actualizacion se 
+	 * devolverá con el valor "false 
+	 */
 	public boolean actualizarStock(int id_Equipamiento, int nuevoStock) {
 		boolean actualizado = true;
 			PreparedStatement pstModificar;
@@ -71,6 +94,17 @@ public class ModeloEquipamiento {
 		return actualizado;
 	}
 
+	/**
+	 * Inserta nuevo equipamiento en la base de datos
+	 * 
+	 * @param nombre es el nombre del nuevo equipamiento
+	 * @param precio es el precio del nuevo equipamiento
+	 * @param stock es el stock del nuevo equipamiento
+	 * @param idClinica es el ID de la clinica en la que se va a insertar el equipamiento
+	 * @return si se inserta correctamente el equipamiento devuelve un boolean llamado
+	 * "insertado" con el valor "true", en caso de que ocurra algun error en la insercion se 
+	 * devolverá con el valor "false 
+	 */
 	public boolean insertar(String nombre, Double precio, int stock, int idClinica) {
 			boolean insertado = true;
 			PreparedStatement pstInsertar;
@@ -89,6 +123,14 @@ public class ModeloEquipamiento {
 			return insertado;
 	}
 
+	/**
+	 * Sirve parta eliminar completamente un equipamiento
+	 * 
+	 * @param delete es el ID del equipamiento que se va a eliminar por completo
+	 * @return si se elimina correctamente el equipamiento devuelve un boolean llamado
+	 * "eliminado" con el valor "true", en caso de que ocurra algun error en la eliminacion se 
+	 * devolverá con el valor "false 
+	 */
 	public boolean eliminarStock(int delete) {
 	    boolean eliminado = false;
 	    try {
@@ -104,6 +146,14 @@ public class ModeloEquipamiento {
 	    return eliminado;
 	}
 
+	/**
+	 * Elimina todo el equipamiento de la clinica que se especifique
+	 * 
+	 * @param id ID de la clinica de la cual se va a eliminar todo el equipamiento
+	 * @return si se elimina correctamente el equipamiento devuelve un boolean llamado
+	 * "eliminado" con el valor "true", en caso de que ocurra algun error en la eliminacion se 
+	 * devolverá con el valor "false 
+	 */
 	public boolean eliminarStocks(int id) {
 		boolean eliminado = true;
 	    PreparedStatement pstDelete;
